e2eb52b33051e995dfa79d7d82333b0277131ea7
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-10-16 21:54:55 +0700

    Mengganti pemanggilan fungsi Greeting dengan fungsi Register pada Scaffold, tetap menggunakan padding dari innerPadding

--- app/src/main/java/com/example/composablelayout/MainActivity.kt
+++ app/src/main/java/com/example/composablelayout/MainActivity.kt
@@ -34,10 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposableLayoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Register(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
