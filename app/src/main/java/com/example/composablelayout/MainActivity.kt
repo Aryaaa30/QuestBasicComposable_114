42cb548dbd2c1c8465a3a672823e3ef8ad0f7415
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-10-16 20:47:11 +0700

    Membuat Anotasi Composable, dan membuat function register beserta dengan parameternya

--- app/src/main/java/com/example/composablelayout/MainActivity.kt
+++ app/src/main/java/com/example/composablelayout/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat.RegisterReceiverFlags
 import com.example.composablelayout.ui.theme.ComposableLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +32,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun Register(modifier: Modifier = Modifier){
 
+}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
